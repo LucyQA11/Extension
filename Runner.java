a680264bd38f0db3427e1da4fee34fa8bb675b43
Your Name <you@example.com>
2020-04-27 11:41:46 +0100

    first commit

---
+++ Runner.java
@@ -0,0 +1,26 @@
+package qa.com.extension;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		
+		
+		Extension db = new Extension();
+		
+//		db.createUser("bob34", "bubbles", "Bob", "Smith");
+//		db.createProduct("bag", 29.99, 23);
+//		db.viewUsers("Users");
+//		db.viewProducts("Products");
+//		db.createOrder(1, 2, 1);
+//		db.updateUser("Users",2 , "John");
+//		db.updateProducts("Products", 2, "Purse");
+//		db.updateOrder("Orders", 4, 1,2);
+		db.viewOrders("Orders");
+		
+		
+		
+		
+				
+	}
+
+}
